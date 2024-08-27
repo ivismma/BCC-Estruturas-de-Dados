124685a6aa8b6f14c252588c708669f24fd9ff2c
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-08-27 18:13:32 -0300

    lista de alocação sequencial

--- Lista de alocação sequencial/lista de alocação sequencial.c
+++ Lista de alocação sequencial/lista de alocação sequencial.c
@@ -10,28 +10,31 @@ typedef struct{
 	int nelem;  // quantidade de elementos atual
 } ListaSeq; // Lista sequencial
 
-void init(ListaSeq *lista, int tamanho); // inicia lista
-int busca(ListaSeq *lista, int valor);   // retorna indice da 1a ocorrencia do valor
-void insere(ListaSeq *lista, int valor); // insere valor
-bool deleta(ListaSeq *lista, int valor); // remove 1a ocorrencia do valor
-void limpar(ListaSeq *lista);  // esvazia a lista (atualiza atual).
-void destroi(ListaSeq *lista); // limpa a lista
-void exibir(ListaSeq lista);   // exibe a lista
+void init(ListaSeq *lista, int tamanho);          // inicia lista.
+int busca(ListaSeq *lista, int valor);            // retorna indice da 1a ocorrencia do valor.
+void insere(ListaSeq *lista, int valor, int pos); // insere valor na pos. como argumento.
+void insereFim(ListaSeq *lista, int valor);       // insere no fim.
+bool deleta(ListaSeq *lista, int valor);          // remove 1a ocorrencia do valor inserido.
+void limpar(ListaSeq *lista);                     // esvazia a lista (atualiza atual).
+void destroi(ListaSeq *lista);                    // limpa a lista.
+void exibir(ListaSeq lista);                      // exibe a lista.
 
 const int INT_SIZE = sizeof(int);
 
 int main(void){
 	ListaSeq lista;
 	lista.l = NULL;
-	
 	init(&lista, 10);
-	insere(&lista, 2);
-	insere(&lista, 1);
-	insere(&lista, 0);
-	insere(&lista, -4);
+	
+	insereFim(&lista, 2);
+	insereFim(&lista, 1);
+	insereFim(&lista, 0);
+	insereFim(&lista, 9);
+	insere(&lista, -4, 1);
 	exibir(lista);
 	deleta(&lista, 1);
 	exibir(lista);
+	
 	destroi(&lista);
 	
 	return 0;
@@ -52,20 +55,35 @@ void init(ListaSeq *lista, int tamanho){
 }
 
 /*
-	ao invés de realocar de tamanho + tamanho,
-	realoca o dobro do tamanho atual para
-	melhorar complexidade de P.A para P.G.	
+	ao invés de realocar de tamanho + tamanho quando
+	necessário, realoca o dobro do tamanho atual para
+	melhorar complexidade de P.A para P.G.
+	Ex.: (30+30+30+...) (P.A) | (30+60+120+...) (P.G)
 */
-void insere(ListaSeq *lista, int valor){
-	if(lista->nelem == lista->tam){
-		int *aux = realloc(lista->l, (lista->tam *= 2));
-		if(aux == NULL){
-			printf("falha na realocacao.\n");
-			exit(3);
-		}
-		else lista->l = aux;
+
+void insereFim(ListaSeq *lista, int valor){
+	insere(lista, valor, lista->nelem);
+}
+
+void insere(ListaSeq *lista, int valor, int pos){
+	if(pos > lista->nelem || pos < 0) 
+		exit(10); // out of range...
+	if(lista->nelem == lista->tam){ 
+		lista->l = (int *) realloc(lista->l, (lista->tam*= 2));
+		if(lista->l == NULL) exit(11); // falha aloc.
 	}
-	lista->l[lista->nelem++] = valor;
+	
+	int *v = lista->l;
+	if(pos == lista->nelem) // insere no fim..
+		v[lista->nelem] = valor;
+	else{
+		int i;
+		// joga todo mundo pra frente (caro)
+		for(i = lista->nelem; i > pos; --i)
+			v[i] = v[i-1];
+		v[i] = valor;
+	}
+	++lista->nelem;
 }
 
 // retorna -1 se n encontrou.
@@ -77,7 +95,6 @@ int busca(ListaSeq *lista, int valor){
 	}
 	return -1;
 }
-
 
 /*
 	Deleta e substitui no lugar do removido
@@ -105,7 +122,7 @@ void destroi(ListaSeq *lista){
 	lista->nelem = 0;
 	lista->tam = 0;
 	free(lista->l);
- lista->l = NULL;
+    lista->l = NULL;
 }
 
 void exibir(ListaSeq lista){
@@ -114,4 +131,3 @@ void exibir(ListaSeq lista){
 		printf("%d ", lista.l[i]);
 	printf("%d]\n", lista.l[lista.nelem-1]);
 }
-
